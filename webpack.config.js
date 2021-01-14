b74913999f2cdc6ac2595e5a7dfedd13eed40ffe
Andrew Vance <andrewaddress2@gmail.com>
2021-01-14 15:18:38 -0800

    current config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const SRC_FILE = path.join(__dirname, 'client/index.jsx');
+const OUT_PATH = path.join(__dirname, 'public');
+
+module.exports = {
+  mode: 'development',
+  entry: SRC_FILE,
+  output: {
+    filename: 'bundle.js',
+    path: OUT_PATH
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
