926d93d81748835e54142c2ed32ae7d4a99936f8
Andrew Vance <andrewaddress2@gmail.com>
2021-01-14 18:30:40 -0800

    now getting similar items based on category on load

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import SimilarItemsList from './SimilarItemsList.jsx';
+import axios from 'axios';
 
 const App = (props) => {
+
+  const [products, updateProducts] = useState([]);
+
+  useEffect(() => {
+    let categoryId = 3;
+
+    axios.get(`/products/similar/${categoryId}`)
+      .then(results => {
+        updateProducts(results.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }, []);
+
   return (
-    <div>a react component</div>
+    <div>
+      <div>Similar Items</div>
+      <div>
+        < SimilarItemsList products={products}/>
+      </div>
+    </div>
   );
 };
 
