6aefade7457d9bd44d024d6b123c3fcf0a046b9e
Andrew Vance <andrewaddress2@gmail.com>
2021-01-14 18:31:31 -0800

    returning a list of similar item entries

---
+++ client/components/SimilarItemsList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import _ from 'lodash';
+import SimilarItemsListEntry from './SimilarItemsListEntry.jsx';
+
+const SimilarItemsList = (props) => {
+  return (
+    _.map(props.products, product => {
+      return < SimilarItemsListEntry key={product.id} product={product} />;
+    })
+  );
+};
+
+export default SimilarItemsList;
