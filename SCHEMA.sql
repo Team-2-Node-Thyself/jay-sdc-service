aed169452b1f00a39ff5504306810799090bbfc7
Andrew Vance <andrewaddress2@gmail.com>
2021-01-14 19:00:37 -0800

    changed decimal values to string to keep zeros after decimal point

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS products (
   imageUrl VARCHAR(120),
   category INT,
   isFavorite INT,
-  price DECIMAL(4,2),
-  cutPrice DECIMAL(4,2),
-  rating DECIMAL(2,1),
+  price VARCHAR(12),
+  cutPrice VARCHAR(12),
+  rating VARCHAR(12),
   reviewCount INT
 );
 
